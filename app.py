f67b442775b22f85a4a4bc113f52cb85bd651ed4
ErikBoesen <me@erikboesen.com>
2019-05-16 18:07:15 -0400

    Add templates

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/control")
+def control():
+    return render_template("control.html")
+
+
+@app.route("/send", methods=["POST"])
+def send():
+
