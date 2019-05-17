b05070df3ef0f67045e63f2ec7bd527773fd7340
ErikBoesen <me@erikboesen.com>
2019-05-17 18:26:36 -0400

    Use different sections so that clicking isn't arbitrary

--- static/scripts/receive.js
+++ static/scripts/receive.js
@@ -1,6 +1,8 @@
 var sound = new Audio("/static/sound.wav");
 
-onclick = function() {
+document.getElementById("accept").onclick = function() {
+    document.getElementById("information").style.display = "none";
+    document.getElementById("preparation").style.display = "block";
     socket.on("play", function() {
         console.log("Playing sound.");
         sound.play().catch(function(e) { console.log(e) });
