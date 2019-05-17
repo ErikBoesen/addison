9f97aab278f319f3f0b4e7b06d1c8ce8c5e95fe4
ErikBoesen <me@erikboesen.com>
2019-05-16 21:28:43 -0400

    Fix refusal to play due to lack of user interaction maybe

--- static/scripts/receive.js
+++ static/scripts/receive.js
@@ -1,6 +1,8 @@
 var sound = new Audio("/static/sound.wav");
 
-socket.on("play", function() {
-    console.log("Playing sound.");
-    sound.play().catch(function(e) { console.log(e) });
-});
+onclick = function() {
+    socket.on("play", function() {
+        console.log("Playing sound.");
+        sound.play().catch(function(e) { console.log(e) });
+    });
+};
